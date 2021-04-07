b945d734680aa681bad53ed9b353893374a77272
George <hatzi249@gmail.com>
2021-04-07 12:06:51 +0200

    h

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ const express = require('express')
 const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+  debug: true
+});
 const { v4: uuidV4 } = require('uuid')
+
+app.use('/peerjs', peerServer);
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
@@ -26,4 +32,4 @@ io.on('connection', socket => {
   })
 })
 
-server.listen(process.env.PORT||3000)
+server.listen(process.env.PORT||3030)
