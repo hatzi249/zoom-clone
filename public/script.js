a0cb48fb529e3589a8e6760f2d49b0db43447c52
George <58467708+hatzi249@users.noreply.github.com>
2021-04-23 13:18:45 +0200

    Update script.js

--- public/script.js
+++ public/script.js
@@ -8,7 +8,7 @@ const myPeer = new Peer(undefined, {
 const myVideo = document.createElement('video')
 myVideo.muted = true
 const peers = {}
-navigator.mediaDevices.getUserMedia({
+navigator.mediaDevices.getDisplayMedia({
   video: true,
   audio: true
 }).then(stream => {
